d2f787f7a58c3b2ac55d2cb584f49765c629a78d
Lenovo-User <Lenovo-User@DESKTOP-7L4SHKG>
2024-01-19 17:01:35 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 25
+        ListBox1.Location = New Point(190, 144)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(369, 129)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.White
+        Label1.Location = New Point(188, 287)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(276, 94)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(204, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(190, 57)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(393, 31)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(190, 330)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(49, 25)
+        Label2.TabIndex = 4
+        Label2.Text = "Case"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(459, 330)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(65, 25)
+        Label3.TabIndex = 5
+        Label3.Text = "Colour"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(188, 358)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(109, 29)
+        RadioButton1.TabIndex = 6
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "To Upper"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(188, 393)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(107, 29)
+        RadioButton2.TabIndex = 6
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "To Lower"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(459, 358)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(87, 29)
+        RadioButton3.TabIndex = 6
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Purple"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(459, 393)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(70, 29)
+        RadioButton4.TabIndex = 6
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Blue"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(190, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(112, 25)
+        Label4.TabIndex = 7
+        Label4.Text = "Enter a word"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(RadioButton4)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label4 As Label
+
+End Class
